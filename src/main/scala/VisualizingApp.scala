c3c8c6c8f6201d7b9e0997acb9aaab9ba394626d
Dmitry Openkov <dimaopen@gmail.com>
2021-03-22 16:10:36 +0300

    Confirm file overwriting

--- src/main/scala/VisualizingApp.scala
+++ src/main/scala/VisualizingApp.scala
@@ -6,7 +6,7 @@ import fs2._
 import scopt.{OEffect, OParser}
 
 import java.io.File
-import java.nio.file.{NoSuchFileException, Path, Paths}
+import java.nio.file.{Files, NoSuchFileException, Path, Paths}
 
 /**
  * @author Dmitry Openkov
@@ -19,17 +19,57 @@ object VisualizingApp extends IOApp {
         ExitCode.Error
       }
       case Right(cliOptions) =>
-        Blocker[IO].use { blocker =>
-          val inputFile = cliOptions.input
-          val csvStream: Stream[IO, MessageReader.RowData] = MessageReader.readData[IO](inputFile, blocker)
-          val puml: Stream[IO, MessageSequenceProcessor.PumlEntry] = MessageSequenceProcessor.processMessages(csvStream)
-          PumlWriter.writeData[IO](puml, cliOptions.output, blocker).compile.drain.as(ExitCode.Success)
-            .handleErrorWith {
-              case _: NoSuchFileException => IO.delay(println(s"File not found: $inputFile")).as(ExitCode.Error)
-              case x: Throwable => IO.delay(println(s"Error: ${x.getMessage}")).as(ExitCode.Error)
-            }
-        }
+        for {
+          confirm <- confirmOverwrite(cliOptions.output)
+          exitCode <- if (confirm) doJob(cliOptions.input, cliOptions.output) else IO.delay {
+            println("Exiting...")
+            ExitCode.Error
+          }
+        } yield exitCode
 
+
+    }
+  }
+
+  private def doJob(inputFile: Path, output: Path): IO[ExitCode] = {
+    Blocker[IO].use { blocker =>
+      val csvStream: Stream[IO, MessageReader.RowData] = MessageReader.readData[IO](inputFile, blocker)
+      val puml: Stream[IO, MessageSequenceProcessor.PumlEntry] = MessageSequenceProcessor.processMessages(csvStream)
+      PumlWriter.writeData[IO](puml, output, blocker).compile.drain.as(ExitCode.Success)
+        .handleErrorWith {
+          case _: NoSuchFileException => IO.delay(println(s"File not found: $inputFile")).as(ExitCode.Error)
+          case x: Throwable => IO.delay(println(s"Error: ${x.getMessage}")).as(ExitCode.Error)
+        }
+    }
+  }
+
+  private def confirmOverwrite(path: Path): IO[Boolean] = {
+    for {
+      exists <- IO.delay(Files.exists(path))
+      overwrite <- if (exists) askUserYesNoQuestion("File exits. Overwrite? (Y/n)", default = true) else IO.pure(true)
+    } yield overwrite
+  }
+
+  private def askUserYesNoQuestion(question: String, default: Boolean): IO[Boolean] = {
+    for {
+      _ <- IO.delay(println(question))
+      value <- IO.delay(scala.io.StdIn.readLine())
+      answer = if (value.trim.isEmpty) default.some else parseYesNoString(value)
+      result <- answer match {
+        case Some(value) => IO.pure(value)
+        case None => askUserYesNoQuestion(question, default)
+      }
+    } yield result
+
+  }
+
+  private def parseYesNoString(str: String): Option[Boolean] = {
+    str.trim.toLowerCase match {
+      case "y" => true.some
+      case "yes" => true.some
+      case "n" => false.some
+      case "no" => false.some
+      case _ => None
     }
   }
 
@@ -59,4 +99,5 @@ object VisualizingApp extends IOApp {
   }
 
   case class CliOptions(input: Path = Paths.get("."), output: Path = Paths.get("."))
+
 }
